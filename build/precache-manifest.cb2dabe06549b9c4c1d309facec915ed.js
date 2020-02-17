158c7184dc0a5f3324b9503422a39db7e55c6d16
Andrey Artemenko <35538134+Andrew61-20@users.noreply.github.com>
2020-02-17 21:54:37 +0200

    Add files via upload

---
+++ build/precache-manifest.cb2dabe06549b9c4c1d309facec915ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b3d2840406e6e700c54",
+    "url": "/Phones/static/js/runtime~main.8b3d2840.js"
+  },
+  {
+    "revision": "c4e57d62ada6fd78a5b0",
+    "url": "/Phones/static/js/main.c4e57d62.chunk.js"
+  },
+  {
+    "revision": "c88180642f1e6a2f9996",
+    "url": "/Phones/static/js/1.c8818064.chunk.js"
+  },
+  {
+    "revision": "c4e57d62ada6fd78a5b0",
+    "url": "/Phones/static/css/main.48b889f6.chunk.css"
+  },
+  {
+    "revision": "c88180642f1e6a2f9996",
+    "url": "/Phones/static/css/1.cd5ac0de.chunk.css"
+  },
+  {
+    "revision": "39a1f0064171defaf8f1cee6aa2f0ee9",
+    "url": "/Phones/index.html"
+  }
+];
